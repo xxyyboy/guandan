a4cb75b0d03314aee6246004f3ac7153fe02f253
746505972 <746505972@qq.com>
2025-04-07 21:12:57 +0800

    Updated 开发日志

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@
 from setuptools import setup
 from Cython.Build import cythonize
 
-setup(ext_modules=cythonize("c_give_cards.pyx", language_level="3"),)
-setup(ext_modules=cythonize("c_rule.pyx", language_level="3"),)
+setup(ext_modules=cythonize(["c_give_cards.pyx"], language_level="3"))
+setup(ext_modules=cythonize("c_rule.pyx", language_level="3"))
