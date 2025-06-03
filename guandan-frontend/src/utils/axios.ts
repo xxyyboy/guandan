ee82a238b8d6dc53692b40cf4146d1c3701f3aba
746505972 <746505972@qq.com>
2025-06-03 16:06:47 +0800

    update README

--- guandan-frontend/src/utils/axios.ts
+++ guandan-frontend/src/utils/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const API_BASE = 'https://precious-ideally-ostrich.ngrok-free.app'
+export const API_BASE = 'http://localhost:8000'
 // ngrok http --url=precious-ideally-ostrich.ngrok-free.app 8000
 // http://localhost:8000
 // https://precious-ideally-ostrich.ngrok-free.app
